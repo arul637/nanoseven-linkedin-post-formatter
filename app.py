b015f69193fa5fdf1c65a79e3229fd0014c96127
Arulkumaran S <sarulkumaran.21042004@gmail.com>
2026-08-08 13:11:30 +0530

    updating ui

--- app.py
+++ app.py
@@ -81,5 +81,5 @@ def plain_text():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5001))
+    port = int(os.environ.get("PORT", 5002))
     app.run(host="0.0.0.0", port=port, debug=True)
